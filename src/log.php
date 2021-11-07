d813b714d44955f866ae741c45d7b97b52c8a2ff
Abdallah Monzer <presskeyword@gmail.com>
2021-11-07 20:01:25 +0200

    change log.php namespaces, delete index.php

--- src/log.php
+++ src/log.php
@@ -2,46 +2,44 @@
 
 namespace amonzer\logs;
 
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
 class log{
 
-    public function __construct()
-    {
-        require __DIR__ . '/vendor/autoload.php';
-    }
-
     public static function info($logs){
-        $log = new Monolog\Logger('name');
-        $log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+        $log = new Logger('name');
+        $log->pushHandler(new StreamHandler('app.log', Logger::WARNING));
         $log->addInfo($logs);
     }
 
     public static function alert($logs){
-        $log = new Monolog\Logger('name');
-        $log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+        $log = new Logger('name');
+        $log->pushHandler(new StreamHandler('app.log', Logger::WARNING));
         $log->addAlert($logs);
     }
 
     public static function critical($logs){
-        $log = new Monolog\Logger('name');
-        $log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+        $log = new Logger('name');
+        $log->pushHandler(new StreamHandler('app.log', Logger::WARNING));
         $log->addCritical($logs);
     }
 
     public static function warning($logs){
-        $log = new Monolog\Logger('name');
-        $log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+        $log = new Logger('name');
+        $log->pushHandler(new StreamHandler('app.log', Logger::WARNING));
         $log->addWarning($logs);
     }
 
     public static function error($logs){
-        $log = new Monolog\Logger('name');
-        $log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+        $log = new Logger('name');
+        $log->pushHandler(new StreamHandler('app.log', Logger::WARNING));
         $log->addError($logs);
     }
 
     public static function debug($logs){
-        $log = new Monolog\Logger('name');
-        $log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+        $log = new Logger('name');
+        $log->pushHandler(new StreamHandler('app.log', Logger::WARNING));
         $log->addDebug($logs);
     }
 
