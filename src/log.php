d3cf9db5ec9df87b7a8e32edcba1747e0bd7a2f5
Abdallah Monzer <presskeyword@gmail.com>
2021-11-07 20:08:51 +0200

    static file extension

--- src/log.php
+++ src/log.php
@@ -7,25 +7,25 @@ use Monolog\Handler\StreamHandler;
 
 class log{
 
-    public static function alert($logs, $file = 'log.log'){
+    public static function alert($logs, $file = 'log'){
         $log = new Logger('name');
         $log->pushHandler(new StreamHandler($file, Logger::WARNING));
         $log->addAlert($logs);
     }
 
-    public static function critical($logs, $file = 'log.log'){
+    public static function critical($logs, $file = 'log'){
         $log = new Logger('name');
         $log->pushHandler(new StreamHandler($file, Logger::WARNING));
         $log->addCritical($logs);
     }
 
-    public static function warning($logs, $file = 'log.log'){
+    public static function warning($logs, $file = 'log'){
         $log = new Logger('name');
         $log->pushHandler(new StreamHandler($file, Logger::WARNING));
         $log->addWarning($logs);
     }
 
-    public static function error($logs, $file = 'log.log'){
+    public static function error($logs, $file = 'log'){
         $log = new Logger('name');
         $log->pushHandler(new StreamHandler($file, Logger::WARNING));
         $log->addError($logs);
