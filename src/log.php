ee4b8024470ca869b787ca0cbe5c7ec272fd18cc
Abdallah Monzer <presskeyword@gmail.com>
2021-11-07 20:09:09 +0200

    static file extension

--- src/log.php
+++ src/log.php
@@ -9,25 +9,25 @@ class log{
 
     public static function alert($logs, $file = 'log'){
         $log = new Logger('name');
-        $log->pushHandler(new StreamHandler($file, Logger::WARNING));
+        $log->pushHandler(new StreamHandler($file.'.log', Logger::WARNING));
         $log->addAlert($logs);
     }
 
     public static function critical($logs, $file = 'log'){
         $log = new Logger('name');
-        $log->pushHandler(new StreamHandler($file, Logger::WARNING));
+        $log->pushHandler(new StreamHandler($file.'.log', Logger::WARNING));
         $log->addCritical($logs);
     }
 
     public static function warning($logs, $file = 'log'){
         $log = new Logger('name');
-        $log->pushHandler(new StreamHandler($file, Logger::WARNING));
+        $log->pushHandler(new StreamHandler($file.'.log', Logger::WARNING));
         $log->addWarning($logs);
     }
 
     public static function error($logs, $file = 'log'){
         $log = new Logger('name');
-        $log->pushHandler(new StreamHandler($file, Logger::WARNING));
+        $log->pushHandler(new StreamHandler($file.'.log', Logger::WARNING));
         $log->addError($logs);
     }
 
