607e9f97619cc952b5b72892bd1ac59ef44db51a
Abdallah Monzer <presskeyword@gmail.com>
2021-11-07 18:52:24 +0200

    add Log.php class

---
+++ Log.php
@@ -0,0 +1,46 @@
+<?php
+
+class Log{
+
+    public function __construct()
+    {
+        require __DIR__ . '/vendor/autoload.php';
+    }
+
+    public static function info($logs){
+        $log = new Monolog\Logger('name');
+        $log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+        $log->addInfo($logs);
+    }
+
+    public static function alert($logs){
+        $log = new Monolog\Logger('name');
+        $log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+        $log->addAlert($logs);
+    }
+
+    public static function critical($logs){
+        $log = new Monolog\Logger('name');
+        $log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+        $log->addCritical($logs);
+    }
+
+    public static function warning($logs){
+        $log = new Monolog\Logger('name');
+        $log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+        $log->addWarning($logs);
+    }
+
+    public static function error($logs){
+        $log = new Monolog\Logger('name');
+        $log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+        $log->addError($logs);
+    }
+
+    public static function debug($logs){
+        $log = new Monolog\Logger('name');
+        $log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+        $log->addDebug($logs);
+    }
+
+}
